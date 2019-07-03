72cb50c446812effe6cd4619a6344c395b1ec062
devssun <chocochic85@gmail.com>
2019-07-04 00:21:37 +0900

    [TEST] Check Current Number Green Cycle

--- FizzBuzzTDD/FizzBuzzTDDTests/FizzBuzzTDDTests.swift
+++ FizzBuzzTDD/FizzBuzzTDDTests/FizzBuzzTDDTests.swift
@@ -15,6 +15,18 @@ class FizzBuzz {
         self.number = number
     }
     
+    func checkCurrentNumber() -> String {
+        if (number % 3 == 0) && (number % 5 == 0) {
+            return "FizzBuzz"
+        }else if number % 3 == 0 {
+            return "Fizz"
+        }else if number % 5 == 0 {
+            return "Buzz"
+        }else{
+            return "\(number)"
+        }
+    }
+
     func is3MultipleNumber() -> String {
         return number % 3 == 0 ? "Fizz" : ""
     }
